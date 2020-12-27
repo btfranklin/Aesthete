d99292bc973c59832be92d636212d7dd0d35fffc
B.T. Franklin <brandon.franklin@gmail.com>
2020-12-27 00:19:59 -0700

    Added CGPoint extension to get random values inside a CGRect. Also added the associated unit test.

--- Sources/Aesthete/Extensions/CGPoint+random.swift
+++ Sources/Aesthete/Extensions/CGPoint+random.swift
@@ -5,14 +5,19 @@ import CoreGraphics
 extension CGPoint {
     
     public static func random(inX xRange: ClosedRange<CGFloat>, inY yRange: ClosedRange<CGFloat>) -> CGPoint {
-        return CGPoint(x: CGFloat.random(in: xRange), y: CGFloat.random(in: yRange))
+        CGPoint(x: CGFloat.random(in: xRange), y: CGFloat.random(in: yRange))
     }
     
     public static func random(inX xRange: ClosedRange<Double>, inY yRange: ClosedRange<Double>) -> CGPoint {
-        return CGPoint(x: Double.random(in: xRange), y:  Double.random(in: yRange))
+        CGPoint(x: Double.random(in: xRange), y:  Double.random(in: yRange))
     }
     
     public static func random(inX xRange: ClosedRange<Int>, inY yRange: ClosedRange<Int>) -> CGPoint {
-        return CGPoint(x: Int.random(in: xRange), y:  Int.random(in: yRange))
+        CGPoint(x: Int.random(in: xRange), y:  Int.random(in: yRange))
     }
+
+    public static func random(in rect: CGRect) -> CGPoint {
+        CGPoint.random(inX: rect.minX...rect.maxX, inY: rect.minY...rect.maxY)
+    }
+
 }
---
+++ Tests/AestheteTests/Extensions/CGPointExtensionTests.swift
@@ -0,0 +1,37 @@
+//  Created by B.T. Franklin on 12/27/20.
+
+import Foundation
+
+import XCTest
+@testable import Aesthete
+
+class CGPointExtensionTests: XCTestCase {
+
+    func testRandom() {
+
+        let defaultRect = CGRect()
+        for _ in 0...1000 {
+            let point = CGPoint.random(in: defaultRect)
+            XCTAssertEqual(.zero, point)
+        }
+
+        let unitRect = CGRect(origin: .zero, size: CGSize(width: 1, height: 1))
+        for _ in 0...1000 {
+            let point = CGPoint.random(in: unitRect)
+            XCTAssertTrue(unitRect.contains(point))
+        }
+
+        let negativeRect = CGRect(origin: .zero, size: CGSize(width: -3, height: -2))
+        for _ in 0...1000 {
+            let point = CGPoint.random(in: negativeRect)
+            XCTAssertTrue(negativeRect.contains(point))
+        }
+
+        let complexRect = CGRect(x: -0.2124445, y: 23.0002432, width: 12.322304, height: 0.124296875)
+        for _ in 0...1000 {
+            let point = CGPoint.random(in: complexRect)
+            XCTAssertTrue(complexRect.contains(point))
+        }
+
+    }
+}
