a1d34afcfb6e83e49dec375011ef3d5fd31b04dc
B.T. Franklin <brandon.franklin@gmail.com>
2021-02-21 14:16:54 -0700

    Reorganized the folder hierarchy a little bit to split out "Paths" from "Forms".

--- Sources/Aesthete/Forms/RepeatableForm.swift
+++ Sources/Aesthete/Forms/RepeatableForm.swift
@@ -35,7 +35,7 @@ public struct RepeatableForm: Codable {
         
         self.pathlets = pathlets
     }
-    
+
     public func createPath() -> CGPath {
         
         let path = CGMutablePath()
