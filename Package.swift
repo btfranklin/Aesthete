6c22610a14e5dd1b09c2f1a39ce9df49e8b3e57a
B.T. Franklin <brandon.franklin@gmail.com>
2021-12-19 20:19:39 -0700

    Updated ControlledChaos dependency version in the package file.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/btfranklin/ControlledChaos",
-                    .upToNextMajor(from: "1.0.0")),
+                    .upToNextMajor(from: "1.1.0")),
     ],
     targets: [
         .target(
